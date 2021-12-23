32336c8fb039bc9697613b4b9b36c56e345402a8
Anton Yip <Anton_Yip@hotmail.com>
2021-12-23 17:42:04 +0800

    progress

--- src/views/dfk_report.js
+++ src/views/dfk_report.js
@@ -22,23 +22,56 @@ import React, { useEffect, useState } from "react";
 import {
   TabContent,
   TabPane,
+  Table,
+  Row
 } from "reactstrap";
 import classnames from 'classnames';
+
+function QuestRewards(props){
+
+
+  //console.log('a');
+  if (props.data == '')
+    return (<div>Loading...</div>);
+
+  var html = "<div>";
+  props.data.data.forEach(element => {
+    html += "<div>";
+    //console.log(element);
+    html += element.BLOCK_TIMESTAMP;
+    html += "</div>";
+  });
+  html += "</div>";
+  
+  return (
+    <div><div>hello from quest rewards</div> {html} </div>
+    );
+
+}
 
 function Dfk_Report() {
 
   const [activeTab, setActiveTab] = useState('1');
+  const [questData, setQuestData] = useState('');
 
   const toggle = tab => {
     if(activeTab !== tab) setActiveTab(tab);
   }
 
+  useEffect(async () => {
+    const result = await axios.get("http://dfkreport.antonyip.com/quest-rewards?q=0x0ba43bae4613e03492e4c17af3b014b6c3202b9d")
+    .then( res => {
+      setQuestData(res);
+    })
+  }, []);
+  
   return (
     <>
       <div className="content">
         <TabContent activeTab={activeTab}>
           <TabPane tabId='1'>
             hello world
+            <QuestRewards data={questData}></QuestRewards>
           </TabPane>
         </TabContent>
       </div>
